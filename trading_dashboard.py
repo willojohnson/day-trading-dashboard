5eb0c2b175db116100e28d3d284d10d818eb3645
willojohnson <willojohnson@gmail.com>
2025-07-16 12:07:32 -0400

    Update trading_dashboard.py

--- trading_dashboard.py
+++ trading_dashboard.py
@@ -5,7 +5,7 @@ import datetime
 from streamlit_autorefresh import st_autorefresh
 
 # --- Tickers to Monitor ---
-TICKERS = ["NVDA", "MSFT", "GOOGL", "AMZN", "META", "TSLA", "SNOW", "AI", "AMD", "BBAI", "SOUN", "CRSP", "TSM", "DDOG"]
+TICKERS = ["NVDA", "MSFT", "GOOGL", "AMZN", "META", "TSLA", "SNOW", "AI", "AMD", "BBAI", "SOUN", "CRSP", "TSM", "DDOG", "BTSG"]
 
 # --- Page Setup ---
 st.set_page_config(layout="wide")
